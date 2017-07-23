0cf3347c2265fb5b3f130eb75d4d0b83313e2dbc
z-m-c <1903150733@qq.com>
2017-07-23 18:01:28 +0800

    加了定时器3秒后才会调相应处理程序来测试和res比谁先执行

--- router.js
+++ router.js
@@ -4,7 +4,8 @@ function route(pathname, handle){
  	//检查给定的路径对应的请求处理程序是否存在  
   if(typeof handle[pathname] === 'function'){
   	//存在，直接调用相应的函数
-    handle[pathname]();
+  	setTimeout(function(){handle[pathname]()}, 3000);
+    /*handle[pathname]();*/
   } else{
     console.log('对于' + pathname + '没有找到相应的处理程序！');
   }
