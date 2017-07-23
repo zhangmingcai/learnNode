151998f5139a7fd3831620b1d140b48da91ab4fa
z-m-c <1903150733@qq.com>
2017-07-23 17:48:27 +0800

    只管判断请求内容让谁去做

--- router.js
+++ router.js
@@ -1,4 +1,12 @@
-function route(pathname){
+function route(pathname, handle){
   console.log("路由接收来自url:"+ pathname +"的请求");
+
+ 	//检查给定的路径对应的请求处理程序是否存在  
+  if(typeof handle[pathname] === 'function'){
+  	//存在，直接调用相应的函数
+    handle[pathname]();
+  } else{
+    console.log('对于' + pathname + '没有找到相应的处理程序！');
+  }
 }
 exports.route = route;
