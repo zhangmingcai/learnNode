89808c4cc9d17cf025dda8f5f0b8e66255c87488
z-m-c <1903150733@qq.com>
2017-07-23 17:46:40 +0800

    只管启动引入各个模块

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var server = require("./server");//使用server模块
 var router = require("./router");
+var router = require("./router");
+
+var reqHandlers = require("./requestHandlers");
+//建立一个处理请求的function集合对象handle
+var handle = {};
+
+handle["/"] = reqHandlers.start;
+handle["/start"] = reqHandlers.start;
+handle["/upload"] = reqHandlers.upload;
 //调用server下的公共方法
-server.start(router.route);
+server.start(router.route, handle);//将路由函数和处理请求对象handle注入server.js
